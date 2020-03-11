9516c9f9eaa2e9019d23cc696ea492137776a04d
Luigi Santagada <luigi.santagada@live.com>
2020-03-11 10:59:23 +0100

    .

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -3,21 +3,23 @@
  Created:	3/8/2020 1:53:08 AM
  Author:	luigi.santagada
 */
-
+#include <SoftwareSerial.h>
+SoftwareSerial* softwareSerial = new SoftwareSerial(3, 10);
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(600);
+	Serial.begin(9600);
+	softwareSerial->begin(600);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	if (Serial.available() > 0)
+	if (softwareSerial->available() > 0)
 	{
 		char response[4] = {};
 		/*while (Serial.available() > 0) {
 			response.concat((char)Serial.read());
 		}*/
-		Serial.readBytesUntil('*', response, 4);
+		softwareSerial->readBytesUntil('*', response, 4);
 		String e = response;
 		e.trim();
 		if (e != "")
